e69d51bdf2eaea088e68ad56621f1eb39a0bd6f9
Nnamaka <nnamaka7@gmail.com>
2025-01-09 21:15:13 +0100

    added signup and login pages

--- apps/mellon/app/(auth)/signup/page.tsx
+++ apps/mellon/app/(auth)/signup/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
 
-function SignupPage() {
+import {  Command } from "lucide-react"
+
+import { SignUpForm } from "../_components/signupForm"
+
+export default function SignupPage() {
   return (
-    <div>Signup</div>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Command className="size-4" />
+          </div>
+          Mellon
+        </a>
+        <SignUpForm />
+      </div>
+    </div>
   )
 }
-
-export default SignupPage
