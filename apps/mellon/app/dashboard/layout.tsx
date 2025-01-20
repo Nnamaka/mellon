62b19f3737392560d0f5853fd9b6bf74395dbd74
Nnamaka <nnamaka7@gmail.com>
2025-01-20 23:32:56 +0100

    small change

--- apps/mellon/app/dashboard/layout.tsx
+++ apps/mellon/app/dashboard/layout.tsx
@@ -36,14 +36,14 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
             <Separator orientation="vertical" className="mr-2 h-4" />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        </div> */}
       </SidebarInset>
     </SidebarProvider>
   );
